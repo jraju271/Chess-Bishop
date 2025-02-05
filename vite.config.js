d0a9a3605396cad19f8f3d3e67753a419b7bac36
jraju271 <jraju271@gmail.com>
2025-02-05 20:11:27 +0530

    gamereport email js import error

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@react-pdf/renderer'],
+      external: ['@react-pdf/renderer', '@emailjs/browser'],
     },
   },
 })
