268278335a499ef7e76d936df4e8564ae36c8410
jraju271 <jraju271@gmail.com>
2025-02-06 17:39:35 +0530

    vite base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   build: {
     rollupOptions: {
@@ -12,5 +13,5 @@ export default defineConfig({
     assetsDir: 'assets'
   },
   publicDir: 'public',
-})
+});
 
