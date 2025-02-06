5869e9e0281123541a213de9664c62df19eaa87a
jraju271 <jraju271@gmail.com>
2025-02-06 13:25:31 +0530

    vite main.jsx index

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,26 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import { Provider } from 'react-redux'
+// import {Store} from './Middleware/Store.jsx'
+// import './main.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Provider store={Store}>
+//     <App />
+//     </Provider>
+//   </React.StrictMode>,
+// )
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import {Store} from './Middleware/Store.jsx'
+import App from './App'
 import './main.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
-    <Provider store={Store}>
     <App />
-    </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@react-pdf/renderer'],
+      external: ['@react-pdf/renderer', '@emailjs/browser'],
     },
     outDir: 'dist',
     assetsDir: 'assets',
-    modulePreload: true
+    manifest: true,
   },
-  publicDir: 'public',
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript',
+    },
+  },
+  optimizeDeps: {
+    exclude: ['@react-pdf/renderer']
+  }
 })
-
