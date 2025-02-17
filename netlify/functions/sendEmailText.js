441eb518facc02178adbd61403db07ead1b2ef4e
jraju271 <jraju271@gmail.com>
2025-02-17 16:53:00 +0530

    emailsub_time

--- netlify/functions/sendEmailText.js
+++ netlify/functions/sendEmailText.js
@@ -125,7 +125,7 @@ export async function handler(event, context) {
     from: process.env.Mail_User,
     //to: toEmail,
     to: recipients,
-    subject: `Chess Performance Report - ${new Date().toUTCString()}`,
+    subject: `Chess Performance Report - ${new Date().toLocaleTimeString()}`,
     text: emailBody,
   };
 
