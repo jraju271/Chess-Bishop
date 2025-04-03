bf34e791b5d4f2e1450240c9a0a473eea7a39992
Ajaykarthick21 <ajaykarthick1998@gmail.com>
2025-04-03 17:50:36 +0530

    commit

--- src/DATA.jsx
+++
@@ -1,7 +0,0 @@
-export const LESSONDATA=[
-    {
-        LessonTitle:"",
-        Overview:"",
-        
-    }
-]
--- src/Middleware/socket.jsx
+++
@@ -1,5 +0,0 @@
-import { io } from "socket.io-client";
-
-const socket = io("https://multi-player-chess.onrender.com");
-
-export default socket;
